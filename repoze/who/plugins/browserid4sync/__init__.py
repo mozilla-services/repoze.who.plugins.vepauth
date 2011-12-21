456a1a336a6a39d53997ba8a07d305b3dcc4bbed
Ryan Kelly <ryan@rfk.id.au>
2011-12-22 10:45:50 +1100

    Add (untested and most likely incorrect) code for OAuth signatures

--- repoze/who/plugins/browserid4sync/__init__.py
+++ repoze/who/plugins/browserid4sync/__init__.py
@@ -50,6 +50,7 @@ __version__ = "%d.%d.%d%s" % __ver_tuple__
 
 import re
 import fnmatch
+from urlparse import urlparse
 
 from zope.interface import implements
 
@@ -59,9 +60,12 @@ from repoze.who.interfaces import IIdentifier, IAuthenticator, IChallenger
 from repoze.who.utils import resolveDotted
 
 import vep
+from vep.utils import get_assertion_info
 
 from repoze.who.plugins.browserid4sync.tokenmanager import SignedTokenManager
-from repoze.who.plugins.browserid4sync.utils import parse_assertion
+from repoze.who.plugins.browserid4sync.utils import (strings_differ,
+                                                     parse_authz_header,
+                                                     NonceCache)
 
 
 class BrowserID4SyncPlugin(object):
@@ -69,71 +73,134 @@ class BrowserID4SyncPlugin(object):
     implements(IIdentifier, IChallenger, IAuthenticator)
 
     def __init__(self, audiences, token_url=None, token_manager=None,
-                 verifier=None):
+                 verifier=None, nonce_timeout=None):
+        # Fill in default values for any unspecified arguments.
+        # I'm not declaring defaults on the arguments themselves because
+        # we would then have to duplicate those defaults into make_plugin.
         if token_url is None:
             token_url = "/token"
         if token_manager is None:
             token_manager = SignedTokenManager()
         if verifier is None:
             verifier = vep.RemoteVerifier()
+        if nonce_timeout is None:
+            nonce_timeout = 5 * 60
+        # Now we can initialize.
         self.audiences = audiences
         if audiences:
             audience_patterns = map(self._compile_audience_pattern, audiences)
             self._audience_patterns = audience_patterns
         self.token_url = token_url
+        self.token_path = urlparse(token_url).path
         self.token_manager = token_manager
         self.verifier = verifier
+        self.nonce_timeout = nonce_timeout
+        self.nonce_cache = NonceCache(nonce_timeout)
 
     def identify(self, environ):
+        """Extract the authentication info from the request.
+
+        If this is a request to the token-provisioning URL then we extract
+        a posted BrowserID session.  Otherwise we extract the OAuth params
+        from the Authorization header.
+        """
         request = Request(environ)
-        # If we're at the token URL then process the login and
-        # assign a new set of credentials.  This will always result
-        # in setting environ["repoze.who.application"].
         if request.path == self.token_path:
-            # You must provision a token using POST.
-            if request.method != "POST":
-                return self._do_bad_request(request, "must use POST")
-            # Grab the assertion from the POST body.
-            assertion = request.POST.get("assertion")
-            if assertion is None:
-                return self._do_bad_request(request, "no assertion")
-            # Extract the audience, so we can check against wildcards.
-            try:
-                audience = parse_assertion(assertion)["audience"]
-            except (ValueError, KeyError):
-                return self._do_bad_request(request, "invalid assertion")
-            if not self._check_audience(request, audience):
-                msg = "The audience \"%s\" is not recognised" % (audience,)
-                return self._do_bad_request(request, msg)
-            # Verify the assertion and find out who they are.
-            try:
-                data = self.verifier.verify(assertion)
-            except Exception:
-                msg = "Invalid BrowserID assertion"
-                return self._do_bad_request(request, msg)
-            # OK, we can go ahead and issue a token.
-            token, secret = self.token_manager.make_token(data["email"])
-            resp = Response()
-            resp.status = 200
-            resp.content = "oauth_token=%s&oauth_secret=%s" % (token, secret)
-            request.environ["repoze.who.application"] = resp
-            return None
-        # Otherwise, extract OAuth signed request details.
-        # TODO: the whole "signing" thing.
-        return None
+            return self._identify_browserid(request)
+        else:
+            return self._identify_oauth(request)
 
     def remember(self, environ, identity):
+        """Remember the user's identity.
+
+        This is a no-op for this plugin; the client is supposed to remember
+        the provisioned OAuth credentials and re-use them for subsequent
+        requests.
+        """
         return []
 
     def forget(self, environ, identity):
-        return []
+        """Forget the user's identity.
+
+        This simply issues a new WWW-Authenticate challenge, which should
+        cause the client to forget any previously-provisioned credentials.
+        """
+        challenge = "OAuth+VEP token_url=\"%s\"" % (self.token_url,)
+        return [("WWW-Authenticate", challenge)]
 
     def challenge(self, environ, status, app_headers=(), forget_headers=()):
-        # TODO: somewhere we need to send a 401 with WWW-Authenticate: OAuth
-        return None
+        """Challenge the user for credentials.
+
+        This simply sends a 401 response using the WWW-Authenticate field
+        as constructed by forget(). 
+        """
+        headers = self.forget(environ, {})
+        headers.extend(app_headers)
+        headers.extend(forget_headers)
+        if not status.startswith("401 "):
+            status = "401 Unauthorized"
+
+        def challenge_app(environ, start_response):
+            start_response(status, headers)
+            return ["Unauthorized"]
+
+        return challenge_app
 
     def authenticate(self, environ, identity):
-        # TODO: the whole "signing" thing.
+        """Authenticate the extracted identity.
+
+        If this is a request to the token-provisioning URL then we verify
+        a posted BrowserID assertion and exchange it for some OAuth session
+        credentials.  Otherwise we verify a signature from an existing set
+        of OAuth credentials.
+        """
+        request = Request(environ)
+        if request.path == self.token_path:
+            return self._authenticate_browserid(request)
+        else:
+            return self._authenticate_oauth(request)
+
+    #
+    #  Methods for exchanging an assertion for an OAuth session token.
+    #
+
+    def _identify_browserid(self, request): 
+        # You must provision a token using POST.
+        if request.method != "POST":
+            return self._do_bad_request(request, "must use POST")
+        # Grab the assertion from the POST body.
+        assertion = request.POST.get("assertion")
+        if assertion is None:
+            return self._do_bad_request(request, "no assertion")
+        # Extract the audience, so we can check against wildcards.
+        try:
+            audience = get_assertion_info(assertion)["audience"]
+        except (ValueError, KeyError):
+            return self._do_bad_request(request, "invalid assertion")
+        if not self._check_audience(request, audience):
+            msg = "The audience \"%s\" is not recognised" % (audience,)
+            return self._do_bad_request(request, msg)
+        return {
+            "browserid.assertion": assertion,
+            "browserid.audience": audience,
+        }
+
+    def _authenticate_browserid(self, request, identity):
+        assertion = identity.get("browserid.assertion")
+        if not assertion:
+            return assertion
+        # Verify the assertion and find out who they are.
+        try:
+            data = self.verifier.verify(assertion)
+        except Exception:
+            msg = "Invalid BrowserID assertion"
+            return self._do_bad_request(request, msg)
+        # OK, we can go ahead and issue a token.
+        token, secret = self.token_manager.make_token(data["email"])
+        resp = Response()
+        resp.status = 200
+        resp.content = "oauth_token=%s&oauth_secret=%s" % (token, secret)
+        request.environ["repoze.who.application"] = resp
         return None
 
     def _check_audience(self, request, audience):
@@ -156,6 +223,96 @@ class BrowserID4SyncPlugin(object):
         if "://" not in pattern:
             re_pattern = "[a-z]+://" + re_pattern
         return re.compile(re_pattern)
+
+    #
+    #  Methods for Two-Legged OAuth once the assertion has been verified.
+    #
+
+    def _identify_oauth(self, request):
+        """Parse, validate and return the request Authorization header.
+
+        This method grabs the OAuth credentials from the Authorization header
+        and performs some sanity-checks.  If the credentials are missing or
+        malformed then it returns None; if they're ok then they are returned
+        in a dict.
+
+        Note that this method does *not* validate the OAuth signature.
+        """
+        params = parse_authz_header(request, None)
+        if params is None:
+            return None
+        # Check that various parameters are as expected.
+        if params.get("scheme") != "OAuth":
+            return None
+        if params.get("oauth_signature_method") != "HMAC-SHA1":
+            return None
+        if "oauth_consumer_key" not in params:
+            return None
+        # Check the timestamp, reject if too far from current time.
+        try:
+            timestamp = int(params["oauth_timestamp"])
+        except (KeyError, ValueError):
+            return None
+        if abs(timestamp - time.time()) >= self.nonce_timeout:
+            return None
+        # Check that the nonce is not being re-used.
+        nonce = params.get("oauth_nonce")
+        if nonce is None:
+            return None
+        if nonce in self.nonce_cache:
+            return None
+        # OK, they seem like sensible OAuth paramters.
+        return params
+
+    def _authenticate_oauth(self, environ, identity):
+        # We can only authenticate if it has a valid oauth token.
+        token = identity.get("oauth_consumer_key")
+        if not token:
+            return None
+        userid = self.token_manager.get_userid(token)
+        if not userid:
+            return None
+        # Check the two-legged OAuth signature.
+        secret = self.token_manager.get_secret(token)
+        sigdata = self._get_oauth_sigdata(request)
+        expected_sig = b64encode(hmac.new(secret, sigdata, sha1).digest())
+        if strings_differ(identity["oauth_signature"], expected_sig):
+            return None
+        # Cache the nonce to avoid re-use.
+        # We do this *after* successul auth to avoid DOS attacks.
+        nonce = identity["oauth_nonce"]
+        timestamp = int(identity["oauth_timestamp"])
+        self.nonce_cache.add(nonce, timestamp)
+        return userid
+
+    def _get_oauth_sigdata(self, request):
+        """Get the data to be signed for OAuth authentication.
+
+        This method takes a request object and returns the data that should
+        be signed for OAuth authentication of that request.  This data is the
+        "signature base string" as defined in section 3.4.1 of RFC-5849.
+        """
+        bits = []
+        # The request method in upper-case.
+        bits.append(request.method.upper())
+        # The base string URI. TODO: figure out encoding
+        uri = request.path_url
+        host_len = len(request.host_url)
+        uri = uri[:host_len].lower() + uri[host_len:]
+        bits.append(uri)
+        # The request parameters.
+        # TODO: encoding; TODO: omit oauth_signature;
+        params = request.GET.items()
+        params.extend(parse_authz_header(request, {}).items())
+        if request.content_type == "application/x-www-form-urlencoded":
+            params.extend(request.POST.items())
+        params.sort()
+        bits.append("&".join("%s=%s" % (k, v) for k, v in params))
+        return "&".join(bits)
+
+    #
+    #  Misc helper methods.
+    #
 
     def _do_bad_request(self, request, message="Bad Request"):
         assert request.path == self.token_url
--- repoze/who/plugins/browserid4sync/utils.py
+++ repoze/who/plugins/browserid4sync/utils.py
@@ -39,47 +39,8 @@ Helper functions for repoze.who.plugins.browserid4sync.
 
 """
 
-import vep.utils
-
-
-# TODO: this function itself could probably live in vep.utils.
-
-def parse_assertion(assertion):
-    """Parse interesting information out of a BrowserID assertion.
-
-    This function decodes and parses the given BrowserID assertion, returning
-    a dict with the following items:
-
-       * principal:  the asserted identity, eg: {"email": "test@example.com"}
-       * audience:   the audience to whom it is asserted
-
-    This does *not* verify the assertion at all, it is merely a way to see
-    the information that is being asserted.  If the assertion is malformed
-    then ValueError will be raised.
-    """
-    info = {}
-    # Decode the bundled-assertion envelope.
-    try:
-        data = vep.utils.decode_json_bytes(assertion)
-        certificates = data["certificates"]
-        assertion = data["assertion"]
-        # Get the asserted principal out of the certificate chain.
-        info["principal"] = parse_jwt(certificates[0])["principal"]
-        # Get the audience out of the assertion token.
-        info["audience"] = parse_jwt(assertion)["aud"]
-    except (TypeError, KeyError), e:
-        raise ValueError(str(e))
-    return info
-
-
-def parse_jwt(token):
-    """Parse a JWT to get the contained information.
-
-    This function parses a JSON Web Token and returns the contained dict of
-    information.  It does not validate the signature.
-    """
-    payload = token.split(".")[1]
-    return vep.utils.decode_json_bytes(payload)
+import threading
+import heapq
 
 
 def strings_differ(string1, string2):
@@ -99,3 +60,160 @@ def strings_differ(string1, string2):
     for a, b in zip(string1, string2):
         invalid_bits += a != b
     return invalid_bits != 0
+
+
+class NonceCache(object):
+    """Object for managing a short-lived cache of nonce values.
+
+    This class allow easy management of client-generated nonces.  It keeps
+    a set of seen nonce values so that they can be looked up quickly, and
+    a queue ordering them by timestamp so that they can be purged when
+    they expire.
+    """
+
+    def __init__(self, timeout=None):
+        if timeout is None:
+            timeout = 5 * 60
+        self.timeout = timeout
+        self.nonces = set()
+        self.purge_lock = threading.Lock()
+        self.purge_queue = []
+
+    def __contains__(self, nonce):
+        """Check if the given nonce is in the cache."""
+        return (nonce in self.nonces)
+
+    def add(self, nonce, timestamp):
+        """Add the given nonce to the cache."""
+        with self.purge_lock:
+            # Purge a few expired nonces to make room.
+            # Don't purge *all* of them, since we don't want to pause too long.
+            purge_deadline = time.time() - self.timeout
+            try:
+                for _ in xrange(5):
+                    (old_timestamp, old_nonce) = self.purge_queue[0]
+                    if old_timestamp >= purge_deadline:
+                        break
+                    self.nonces.remove(old_nonce)
+                    heapq.heappop(self.purge_queue)
+            except (IndexError, KeyError):
+                pass
+            # Add the new nonce into queue and map.
+            heapq.heappush(self.purge_queue, (timestamp, nonce))
+            self.nonces.add(nonce)
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Sauropod.
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2011
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Ryan Kelly (rkelly@mozilla.com)
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+"""
+
+Helper functions for minimal sauropod server.
+
+"""
+
+import re
+import json
+import urllib2
+
+
+# Regular expression matching a single param in the HTTP_AUTHORIZATION header.
+# This is basically <name>=<value> where <value> can be an unquoted token,
+# an empty quoted string, or a quoted string where the ending quote is *not*
+# preceded by a backslash.
+_AUTH_PARAM_RE = r'([a-zA-Z0-9_\-]+)=(([a-zA-Z0-9_\-]+)|("")|(".*[^\\]"))'
+_AUTH_PARAM_RE = re.compile(r"^\s*" + _AUTH_PARAM_RE + r"\s*$")
+
+# Regular expression matching an unescaped quote character.
+_UNESC_QUOTE_RE = r'(^")|([^\\]")'
+_UNESC_QUOTE_RE = re.compile(_UNESC_QUOTE_RE)
+
+# Regular expression matching a backslash-escaped characer.
+_ESCAPED_CHAR = re.compile(r"\\.")
+
+
+def parse_authz_header(request, *default):
+    """Parse the authorization header into an identity dict.
+
+    This function can be used to extract the Authorization header from a
+    request and parse it into a dict of its constituent parameters.  The
+    auth scheme name will be included under the key "scheme", and any other
+    auth params will appear as keys in the dictionary.
+
+    For example, given the following auth header value:
+
+        'Digest realm="Sync" userame=user1 response="123456"'
+
+    This function will return the following dict:
+
+        {"scheme": "Digest", realm: "Sync",
+         "username": "user1", "response": "123456"}
+
+    """
+    # Grab the auth header from the request, if any.
+    authz = request.environ.get("HTTP_AUTHORIZATION")
+    if authz is None:
+        if default:
+            return default[0]
+        raise ValueError("Missing auth parameters")
+    scheme, kvpairs_str = authz.split(None, 1)
+    # Split the parameters string into individual key=value pairs.
+    # In the simple case we can just split by commas to get each pair.
+    # Unfortunately this will break if one of the values contains a comma.
+    # So if we find a component that isn't a well-formed key=value pair,
+    # then we stitch bits back onto the end of it until it is.
+    kvpairs = []
+    if kvpairs_str:
+        for kvpair in kvpairs_str.split(","):
+            if not kvpairs or _AUTH_PARAM_RE.match(kvpairs[-1]):
+                kvpairs.append(kvpair)
+            else:
+                kvpairs[-1] = kvpairs[-1] + "," + kvpair
+        if not _AUTH_PARAM_RE.match(kvpairs[-1]):
+            if default:
+                return default[0]
+            raise ValueError('Malformed auth parameters')
+    # Now we can just split by the equal-sign to get each key and value.
+    params = {"scheme": scheme}
+    for kvpair in kvpairs:
+        (key, value) = kvpair.strip().split("=", 1)
+        # For quoted strings, remove quotes and backslash-escapes.
+        if value.startswith('"'):
+            value = value[1:-1]
+            if _UNESC_QUOTE_RE.search(value):
+                if default:
+                    return default[0]
+                raise ValueError("Unescaped quote in quoted-string")
+            value = _ESCAPED_CHAR.sub(lambda m: m.group(0)[1], value)
+        params[key] = value
+    return params
